e7f2aee508665149894682527083a72116ace643
saifur rahman <saifuroracle@gmail.com>
2020-10-27 00:45:35 +0600

    updated

--- php/oop/static_property/init.php
+++ php/oop/static_property/init.php
@@ -7,4 +7,5 @@ $obj2 = new Book(1, 'algorithm 101', 'riaj');
 $obj3 = new Book(100, 'algorithm 101', 'john');
 $obj4 = new Book(-1, 'algorithm 101', 'imran');
 
-echo $obj4->getLastId();  
+// echo $obj4->getLastId();  
+echo Book::getLastId();  
