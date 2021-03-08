6fc5076f473bc364ced4bcc99cbe3059ae0bdf2b
saifur rahman <saifuroracle@gmail.com>
2021-03-08 10:52:10 +0600

    array slice and splice added

--- js/array/array_splice.js
+++ js/array/array_splice.js
@@ -1,4 +1,4 @@
-// array.splice(start, end)
+//  array.splice(index, howmany, item1, ....., itemX)
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
---
+++ js/array/array_unshift.js
@@ -0,0 +1,13 @@
+//  array.unshift(item1, item2, ..., itemX)
+// The splice() method adds/removes items to/from an array, and returns the removed item(s).
+// Note: This method changes the original array.
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(arr)
+
+arr.unshift(-1,-2)  
+
+console.log(arr) 
+
