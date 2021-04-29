950f67ff139f6d5cb9dcb8eb34b31542c72c62f8
saifur rahman <saifuroracle@gmail.com>
2021-04-30 00:10:50 +0600

    python added

---
+++ python/basic/6.type_casting.py
@@ -0,0 +1,10 @@
+x = input("enter number 1: ")
+y = input("enter number 2: ")
+
+print(x+y)
+print(int(x)+int(y))
+
+sum = int(x)+int(y)
+
+# print("sum = "+ sum)  can't do this
+print("sum = ", sum)
---
+++ python/basic/7.library_functions_math.py
@@ -0,0 +1,14 @@
+from math import *
+
+print(max(1,2,3,4,5))
+print(min(1,2,3,4,5))
+
+print(abs(-5))
+
+print(pow(5, 2))
+print(sqrt(25))
+print(int(sqrt(25)))
+
+print(round(5.5))
+print(floor(5.5))
+print(ceil(5.5))
